d38f77007fa7ff83621329085f145022a078a417
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-07-02 14:38:50 +0900

    Add content script

--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,1 +1,9 @@
-console.log('Hello from the content-script')
+import * as utils from 'n-yobiko-utils';
+
+chrome.runtime.onMessage.addListener(
+    function({ method }, sender, sendResponse) {
+        if (method === 'getDetail') {
+            sendResponse(utils.getChapterDetail());
+        }
+    }
+);
