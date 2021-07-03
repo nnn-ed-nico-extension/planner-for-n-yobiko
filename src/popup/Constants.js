69e293c8b660fddfec3263d801bcd5da03aa378c
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-07-03 10:26:32 +0900

    Add constant and update permisions

---
+++ src/popup/Constants.js
@@ -0,0 +1,8 @@
+module.exports.resourceType = {
+    MOVIE: 'movie',
+    EVALUATION_TEST: 'evaluation_test',
+    ESSAY_TEST: 'essay_test',
+    EVALUATION_REPORT: 'evaluation_report',
+    ESSAY_REPORT: 'essay_report',
+    VR_EXPERIENCE:'vr_experience'
+}
