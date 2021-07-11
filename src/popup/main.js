d4cbb76c6e770cc02f3f6090ae8e3decef4c9c40
tsutoringo <33830878+tsutoringo@users.noreply.github.com>
2021-07-11 18:55:09 +0900

    ✨ fontawesome 追加

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import { faCheckCircle, faDonate } from '@fortawesome/free-solid-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faCheckCircle, faGithub, faDonate);
+
+Vue.component('fa', FontAwesomeIcon)
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+	el: '#app',
+	render: h => h(App)
+});
